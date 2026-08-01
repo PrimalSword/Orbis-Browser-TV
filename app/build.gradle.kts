ec654f60870b8347b07dfb6f8ec4fc8b4a320f80
PrimalSword <106413088+PrimalSword@users.noreply.github.com>
2026-08-01 10:57:29 -0300

    Bump version to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.orbis.browser.tv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
     }
 
     buildTypes {
